03d4b569acdea42fc25c8aeeb81cf4a2a379b727
BeeVan <the.michael.barrell@gmail.com>
2020-07-31 12:32:21 +1000

    sb changed to stringBuilder in: Member.java lines 29, 30, 39, and 41

--- src/library/entities/Member.java
+++ src/library/entities/Member.java
@@ -26,8 +26,8 @@ public class Member implements Serializable {
     }
     
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Member:  ").append(memberId).append("\n")
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Member:  ").append(memberId).append("\n")
                 .append("  Name:  ").append(lastName).append(", ").append(firstName).append("\n")
                 .append("  Email: ").append(emailAddress).append("\n")
                 .append("  Phone: ").append(phoneNumber)
@@ -36,9 +36,9 @@ public class Member implements Serializable {
                 .append("\n");
         
         for (Loan LoAn : currentLoans.values()) {
-            sb.append(LoAn).append("\n");
+            stringBuilder.append(LoAn).append("\n");
         }
-        return sb.toString();
+        return stringBuilder.toString();
     }
     
     public int GeT_ID() {
