44b7c3551755e4516a6da0e90e5a7c47b74e6a05
Firedingo <nay_here@hotmail.com>
2020-08-10 14:53:01 +1000

    Fixed a few tabs that weren't convert to 4 spaces for Indentation compliance

--- src/library/entities/Book.java
+++ src/library/entities/Book.java
@@ -4,93 +4,93 @@ import java.io.Serializable;
 
 @SuppressWarnings("serial")
 public class Book implements Serializable {
-	
+    
     private String title;
     private String author;
     private String callNo;
     private int id;
-	
+    
     private enum State { AVAILABLE, ON_LOAN, DAMAGED, RESERVED };
     private State state;
-	
-	
+    
+    
     public Book(String author, String title, String callNo, int id) {
-	    this.author = author;
-	    this.title = title;
-	    this.callNo = callNo;
-	    this.id = id;
-	    this.state = State.AVAILABLE;
+        this.author = author;
+        this.title = title;
+        this.callNo = callNo;
+        this.id = id;
+        this.state = State.AVAILABLE;
     }
-	
+    
     public String toString() {
-	    StringBuilder sb = new StringBuilder();
-	    sb.append("Book: ").append(id).append("\n")
-		  .append("  Title:  ").append(title).append("\n")
-		  .append("  Author: ").append(author).append("\n")
-		  .append("  CallNo: ").append(callNo).append("\n")
-		  .append("  State:  ").append(state);
-		
-	    return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Book: ").append(id).append("\n")
+          .append("  Title:  ").append(title).append("\n")
+          .append("  Author: ").append(author).append("\n")
+          .append("  CallNo: ").append(callNo).append("\n")
+          .append("  State:  ").append(state);
+        
+        return sb.toString();
     }
 
     public Integer getId() {
-	    return id;
+        return id;
     }
 
     public String getTitle() {
-	    return title;
+        return title;
     }
 
 
-	
+    
     public boolean isAvailable() {
-	    return state == State.AVAILABLE;
+        return state == State.AVAILABLE;
     }
 
-	
+    
     public boolean isOnLoan() {
-	    return state == State.ON_LOAN;
+        return state == State.ON_LOAN;
     }
 
-	
+    
     public boolean isDamaged() {
-	    return state == State.DAMAGED;
+        return state == State.DAMAGED;
     }
 
-	
+    
     public void canBorrow() {
-	    if (state.equals(State.AVAILABLE)) {
-		    state = State.ON_LOAN;
-	    }
-	    else {
-		    throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", state));
-	    }
-		
+        if (state.equals(State.AVAILABLE)) {
+            state = State.ON_LOAN;
+        }
+        else {
+            throw new RuntimeException(String.format("Book: cannot borrow while book is in state: %s", state));
+        }
+        
     }
 
 
     public void canReturn(boolean damaged) {
-	    if (state.equals(State.ON_LOAN)) {
-		    if (damaged) {
-			    state = State.DAMAGED;
-		    }
-		    else {
-			    state = State.AVAILABLE;
-		    }
-	    }
-	    else {
-		    throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", state));
-	    }
+        if (state.equals(State.ON_LOAN)) {
+            if (damaged) {
+                state = State.DAMAGED;
+            }
+            else {
+                state = State.AVAILABLE;
+            }
+        }
+        else {
+            throw new RuntimeException(String.format("Book: cannot Return while book is in state: %s", state));
+        }
     }
 
-	
+    
     public void canRepair() {
-	    if (state.equals(State.DAMAGED)) {
-		    state = State.AVAILABLE;
-	    }
-	    else {
-		    throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", state));
-	    }
+        if (state.equals(State.DAMAGED)) {
+            state = State.AVAILABLE;
+        }
+        else {
+            throw new RuntimeException(String.format("Book: cannot repair while book is in state: %s", state));
+        }
     }
 
 
--- src/library/entities/Loan.java
+++ src/library/entities/Loan.java
@@ -5,74 +5,74 @@ import java.util.Date;
 
 @SuppressWarnings("serial")
 public class Loan implements Serializable {
-	
+    
     public static enum LoanState {CURRENT, OVER_DUE, DISCHARGED};
-	
+    
     private int loanId;
     private Book book;
     private Member member;
     private Date date;
     private LoanState state;
 
-	
+    
     public Loan(int loanId, Book book, Member member, Date dueDate) {
-	    this.loanId = loanId;
-	    this.book = book;
-	    this.member = member;
-	    this.date = dueDate;
-	    this.state = LoanState.CURRENT;
+        this.loanId = loanId;
+        this.book = book;
+        this.member = member;
+        this.date = dueDate;
+        this.state = LoanState.CURRENT;
     }
 
-	
+    
 
     public void checkOverDue() {
-	    if (state == LoanState.CURRENT &&
-		    Calendar.getInstance().getDate().after(date)) {
-		    this.state = LoanState.OVER_DUE;			
-	    }
+        if (state == LoanState.CURRENT &&
+            Calendar.getInstance().getDate().after(date)) {
+            this.state = LoanState.OVER_DUE;            
+        }
     }
 
     public boolean isOverDue() {
-	    return state == LoanState.OVER_DUE;
+        return state == LoanState.OVER_DUE;
     }
 
-	
+    
     public Integer getId() {
-	    return loanId;
+        return loanId;
     }
 
 
     public Date getDueDate() {
-	    return date;
+        return date;
     }
-	
-	
+    
+    
     public String toString() {
-	    SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-	    StringBuilder sb = new StringBuilder();
-	    sb.append("Loan:  ").append(loanId).append("\n")
-		  .append("  Borrower ").append(member.getMemberId()).append(" : ")
-		  .append(member.getLastName()).append(", ").append(member.getFirstName()).append("\n")
-		  .append("  Book ").append(book.getId()).append(" : " )
-		  .append(book.getTitle()).append("\n")
-		  .append("  DueDate: ").append(sdf.format(date)).append("\n")
-		  .append("  State: ").append(state);		
-	    return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append("Loan:  ").append(loanId).append("\n")
+          .append("  Borrower ").append(member.getMemberId()).append(" : ")
+          .append(member.getLastName()).append(", ").append(member.getFirstName()).append("\n")
+          .append("  Book ").append(book.getId()).append(" : " )
+          .append(book.getTitle()).append("\n")
+          .append("  DueDate: ").append(sdf.format(date)).append("\n")
+          .append("  State: ").append(state);        
+        return sb.toString();
     }
 
     public Member getMember() {
-	    return member;
+        return member;
     }
 
 
     public Book getBook() {
-	    return book;
+        return book;
     }
 
 
     public void discharge() {
-	    state = LoanState.DISCHARGED;		
+        state = LoanState.DISCHARGED;        
     }
 
 }
