450d2c3b2454111710dbdfb90a072204a3d79e8b
BeeVan <the.michael.barrell@gmail.com>
2020-08-13 16:18:03 +1000

    FixBookUI.java: Changed mustFix_BoOk to fixBook line 45

--- src/library/fixbook/FixBookUI.java
+++ src/library/fixbook/FixBookUI.java
@@ -42,7 +42,7 @@ public class FixBookUI {
                     boolean mustFix = false;
                     if (answer.toUpperCase().equals("Y"))
                         mustFix = true;
-                    control.mustFix_BoOk(mustFix);
+                    control.fixBook(mustFix);
                     break;
                 case COMPLETED:
                     OuTpUt("Fixing process complete");
