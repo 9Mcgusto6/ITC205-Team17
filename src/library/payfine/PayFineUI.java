1a3287c348f67da157d6286ca9c8cc88f32dbc6c
BeeVan <the.michael.barrell@gmail.com>
2020-08-13 13:48:36 +1000

    PayFineUI.java: Changed CoNtRoL to control lines 7, 12, 29, 34, 44 and 55

--- src/library/payfine/PayFineUI.java
+++ src/library/payfine/PayFineUI.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 public class PayFineUI {
     
     public static enum UI_STATE { INITIALISED, READY, PAYING, COMPLETED, CANCELLED };
-    private PayFineControl CoNtRoL;
+    private PayFineControl control;
     private Scanner input;
     private UI_STATE StAtE;
     
     public PayFineUI(PayFineControl control) {
-        this.CoNtRoL = control;
+        this.control = control;
         input = new Scanner(System.in);
         StAtE = UI_STATE.INITIALISED;
         control.SeT_uI(this);
@@ -26,12 +26,12 @@ public class PayFineUI {
                 case READY:
                     String Mem_Str = input("Swipe member card (press <enter> to cancel): ");
                     if (Mem_Str.length() == 0) {
-                        CoNtRoL.CaNcEl();
+                        control.CaNcEl();
                         break;
                     }
                     try {
                         int Member_ID = Integer.valueOf(Mem_Str).intValue();
-                        CoNtRoL.CaRd_sWiPeD(Member_ID);
+                        control.CaRd_sWiPeD(Member_ID);
                     }
                     catch (NumberFormatException e) {
                         output("Invalid memberId");
@@ -41,7 +41,7 @@ public class PayFineUI {
                     double AmouNT = 0;
                     String Amt_Str = input("Enter amount (<Enter> cancels) : ");
                     if (Amt_Str.length() == 0) {
-                        CoNtRoL.CaNcEl();
+                        control.CaNcEl();
                         break;
                     }
                     try {
@@ -52,7 +52,7 @@ public class PayFineUI {
                         output("Amount must be positive");
                         break;
                     }
-                    CoNtRoL.PaY_FiNe(AmouNT);
+                    control.PaY_FiNe(AmouNT);
                     break;
                 case CANCELLED:
                     output("Pay Fine process cancelled");
