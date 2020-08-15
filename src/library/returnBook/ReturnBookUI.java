5c308a8a4cb0cdb45594356238e26b4ab2bdbba1
Firedingo <nay_here@hotmail.com>
2020-08-15 20:19:26 +1000

    Changed indentation from tabs to 4 spaces in ReturnBookUI class.

--- src/library/returnBook/ReturnBookUI.java
+++ src/library/returnBook/ReturnBookUI.java
@@ -4,85 +4,85 @@ import java.util.Scanner;
 
 public class ReturnBookUI {
 
-	public static enum UiState {INITIALISED, READY, INSPECTING, COMPLETED};
+    public static enum UiState {INITIALISED, READY, INSPECTING, COMPLETED};
 
-	private rETURN_bOOK_cONTROL control;
-	private Scanner input;
-	private UiState state;
+    private rETURN_bOOK_cONTROL control;
+    private Scanner input;
+    private UiState state;
 
-	
-	public ReturnBookUI(rETURN_bOOK_cONTROL control) {
-		this.control = control;
-		input = new Scanner(System.in);
-		state = UiState.INITIALISED;
-		control.sEt_uI(this);
-	}
+    
+    public ReturnBookUI(rETURN_bOOK_cONTROL control) {
+        this.control = control;
+        input = new Scanner(System.in);
+        state = UiState.INITIALISED;
+        control.sEt_uI(this);
+    }
 
 
-	public void run() {		
-		output("Return Book Use Case UI\n");
-		
-		while (true) {
-			
-			switch (state) {
-			
-			case INITIALISED:
-				break;
-				
-			case READY:
-				String bookInputString = input("Scan Book (<enter> completes): ");
-				if (bookInputString.length() == 0) {
-					control.scanningComplete();
-				}
-				else {
-					try {
-						int Book_Id = Integer.valueOf(bookInputString).intValue();
-						control.bookScanned(Book_Id);
-					}
-					catch (NumberFormatException e) {
-						output("Invalid bookId");
-					}					
-				}
-				break;				
-				
-			case INSPECTING:
-				String AnS = input("Is book damaged? (Y/N): ");
-				boolean isDamaged = false;
-				if (AnS.toUpperCase().equals("Y")) {					
-					isDamaged = true;
-				}
-				control.dischargeLoan(isDamaged);
-			
-			case COMPLETED:
-				output("Return processing complete");
-				return;
-			
-			default:
-				output("Unhandled state");
-				throw new RuntimeException("ReturnBookUI : unhandled state :" + state);			
-			}
-		}
-	}
+    public void run() {        
+        output("Return Book Use Case UI\n");
+        
+        while (true) {
+            
+            switch (state) {
+            
+            case INITIALISED:
+                break;
+                
+            case READY:
+                String bookInputString = input("Scan Book (<enter> completes): ");
+                if (bookInputString.length() == 0) {
+                    control.scanningComplete();
+                }
+                else {
+                    try {
+                        int Book_Id = Integer.valueOf(bookInputString).intValue();
+                        control.bookScanned(Book_Id);
+                    }
+                    catch (NumberFormatException e) {
+                        output("Invalid bookId");
+                    }                    
+                }
+                break;                
+                
+            case INSPECTING:
+                String AnS = input("Is book damaged? (Y/N): ");
+                boolean isDamaged = false;
+                if (AnS.toUpperCase().equals("Y")) {                    
+                    isDamaged = true;
+                }
+                control.dischargeLoan(isDamaged);
+            
+            case COMPLETED:
+                output("Return processing complete");
+                return;
+            
+            default:
+                output("Unhandled state");
+                throw new RuntimeException("ReturnBookUI : unhandled state :" + state);            
+            }
+        }
+    }
 
-	
-	private String input(String prompt) {
-		System.out.print(prompt);
-		return input.nextLine();
-	}	
-		
-		
-	private void output(Object object) {
-		System.out.println(object);
-	}
-	
-			
-	public void display(Object object) {
-		output(object);
-	}
-	
-	public void setState(UiState state) {
-		this.state = state;
-	}
+    
+    private String input(String prompt) {
+        System.out.print(prompt);
+        return input.nextLine();
+    }    
+        
+        
+    private void output(Object object) {
+        System.out.println(object);
+    }
+    
+            
+    public void display(Object object) {
+        output(object);
+    }
+    
+    public void setState(UiState state) {
+        this.state = state;
+    }
 
-	
+    
 }
