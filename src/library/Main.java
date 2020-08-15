cba98b53b6d2e10c098a2cf2285698e6cf88ae18
Firedingo <nay_here@hotmail.com>
2020-08-15 22:14:13 +1000

    Changed BoOk to book - Line 204 in Main class.

--- src/library/Main.java
+++ src/library/Main.java
@@ -201,8 +201,8 @@ public class Main {
         String author = input("Enter author: ");
         String title  = input("Enter title: ");
         String callNumber = input("Enter call number: ");
-        Book BoOk = LIB.addBook(author, title, callNumber);
-        output("\n" + BoOk + "\n");
+        Book book = LIB.addBook(author, title, callNumber);
+        output("\n" + book + "\n");
         
     }
 
