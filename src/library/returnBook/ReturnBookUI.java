98d261d2c426b9b2fe06db708a3ad126290fc664
Justin Medley <jt_mcgustin@hotmail.com>
2020-08-20 16:20:16 +1000

    Changed to answer on Line 49 of ReturnBookUI

--- src/library/returnBook/ReturnBookUI.java
+++ src/library/returnBook/ReturnBookUI.java
@@ -46,9 +46,9 @@ public class ReturnBookUI {
                 break;                
                 
             case INSPECTING:
-                String ans = input("Is book damaged? (Y/N): ");
+                String answer = input("Is book damaged? (Y/N): ");
                 boolean isDamaged = false;
-                if (ans.toUpperCase().equals("Y")) {                    
+                if (answer.toUpperCase().equals("Y")) {                    
                     isDamaged = true;
                 }
                 control.dischargeLoan(isDamaged);
